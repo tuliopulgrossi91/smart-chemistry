e00ec68a46e6bc471538bf927071a601aee6ddc1
Tulio Pulgrossi <tuliopulgrossi91@gmail.com>
2019-10-09 10:44:08 -0300

    Update 2
    Musica, audios, paineis no menu principal.

---
+++ Smart Chemistry/Assets/Resources/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject Levels, Settings, Credits, Help;
+
+    public void Select(int s)
+    {
+        switch (s)
+        {
+            case 0: // levels
+                Levels.SetActive(true);
+                break;
+            case 1: // settings
+                Settings.SetActive(true);
+                break;
+            case 2: // credits
+                Credits.SetActive(true);
+                break;
+            case 3: // help
+                Help.SetActive(true);
+                break;
+        }
+    }
+}

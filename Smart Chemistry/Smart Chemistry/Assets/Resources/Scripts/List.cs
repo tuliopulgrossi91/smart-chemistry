b102b5e7277add0b6b02cd13dac9a41b7b3221ca
Tulio Pulgrossi <tuliopulgrossi91@gmail.com>
2019-10-25 21:57:05 -0300

    Update
    Add Camera Shake on Settings, Change GUI Settings, Fix Elements List, Shadow Text, Translate for Portuguese

---
+++ Smart Chemistry/Smart Chemistry/Assets/Resources/Scripts/List.cs
@@ -0,0 +1,95 @@
+﻿
+public class List
+{
+    #region Elements List
+    public static string[] numbers = new string[10] { "Hidrogênio",
+                                                      "16",
+                                                      "Cloro",
+                                                      "Carbono",
+                                                      "10",
+                                                      "Cálcio",
+                                                      "Alumínio",
+                                                      "19",
+                                                      "9",
+                                                      "Nitrogênio" }; // perguntas
+
+    public static string[] numbers_right = new string[10] { "1", "Enxofre", "17", "6", "Neônio", "20", "13", "Potássio", "Flúor", "7" }; // corretas
+    public static string[] numbers_alt1 = new string[10] { "2", "Flúor", "18", "7", "Argônio", "12", "23", "Sódio", "Cloro", "9" }; // alternativas 1
+    public static string[] numbers_alt2 = new string[10] { "3", "Oxigênio", "16", "8", "Nitrogênio", "38", "3", "Carbono", "Bromo", "5" }; // alternativas 2
+
+    public static string[] symbols = new string[10] { "Potássio",
+                                                      "H",
+                                                      "Sódio",
+                                                      "B",
+                                                      "Cobre",
+                                                      "Ferro",
+                                                      "Enxofre",
+                                                      "Mercúrio",
+                                                      "Cálcio",
+                                                      "Neônio" }; // perguntas
+
+    public static string[] symbols_right = new string[10] { "K", "Hidrogênio", "Na", "Boro", "Cu", "Fe", "S", "Hg", "Ca", "Ne" }; // corretas
+    public static string[] symbols_alt1 = new string[10] { "P", "Hélio", "S", "Berílio", "C", "F", "N", "Mo", "Cs", "N" }; // alternativas 1
+    public static string[] symbols_alt2 = new string[10] { "Pt", "Háfnio", "K", "Bromo", "Co", "Fr", "P", "Mc", "C", "Nh" }; // alternativas 2
+
+    public static string[] names = new string[10] { "H",
+                                                    "P",
+                                                    "C",
+                                                    "Elemento radioativo",
+                                                    "N",
+                                                    "Transporta oxigénio pelas hemoglobinas",
+                                                    "F",
+                                                    "Essencial para respiração",
+                                                    "K",
+                                                    "Pb" }; // perguntas
+
+    public static string[] names_right = new string[10] { "Hidrogênio", "Fósforo", "Carbono", "Urânio", "Nitrogênio", "Ferro", "Flúor", "Oxigênio", "Potássio", "Chumbo" }; // corretas
+    public static string[] names_alt1 = new string[10] { "Helio", "Polônio", "Cobre", "Cobalto", "Neônio", "Carbono", "Fósforo", "Nitrogênio", "Cálcio", "Mercúrio" }; // alternativas 1
+    public static string[] names_alt2 = new string[10] { "Háfnio", "Chumbo", "Cobalto", "Lítio", "Nióbio", "Potássio", "Ferro", "Hidrogênio", "Criptônio", "Platina" }; // alternativas 2
+
+    public static string[] mass = new string[10] { "Hidrogênio",
+                                                   "Sódio",
+                                                   "Carbono",
+                                                   "Potássio",
+                                                   "Oxigênio",
+                                                   "Fósforo",
+                                                   "Enxofre",
+                                                   "Hélio",
+                                                   "Nitrogênio",
+                                                   "Cloro" }; // perguntas
+
+    public static string[] mass_right = new string[10] { "1", "23", "12", "39", "16", "31", "32", "4", "14", "35" }; // corretas
+    public static string[] mass_alt1 = new string[10] { "2", "24", "6", "19", "8", "15", "31", "2", "7", "17" }; // alternativas 1
+    public static string[] mass_alt2 = new string[10] { "3", "22", "7", "29", "32", "16", "30", "3", "28", "18" }; // alternativas 2
+
+    public static string[] period = new string[10] { "Hidrogênio",
+                                                     "Potássio",
+                                                     "Carbono",
+                                                     "Ferro",
+                                                     "Hélio",
+                                                     "Cobre",
+                                                     "Oxigênio",
+                                                     "Fósforo",
+                                                     "Cálcio",
+                                                     "Mercúrio"}; // perguntas
+
+    public static int[] period_right = new int[10] { 1, 4, 2, 4, 1, 4, 2, 3, 4, 6 }; // corretas
+    public static int[] period_alt1 = new int[10] { 2, 3, 3, 5, 3, 5, 3, 4, 5, 5 }; // alternativas 1
+    public static int[] period_alt2 = new int[10] { 3, 2, 4, 6, 4, 6, 4, 5, 3, 7 }; // alternativas 2
+
+    public static string[] family = new string[10] { "Representa os Metais Alcalinos",
+                                                     "Cloro",
+                                                     "Representa os Metais Alcalinos Terrosos",
+                                                     "Hélio",
+                                                     "Representa os Calcogênios",
+                                                     "Carbono",
+                                                     "Representa os Gases Nobres",
+                                                     "Boro",
+                                                     "Representa os Halogênios",
+                                                     "Magnésio"}; // perguntas
+
+    public static string[] family_right = new string[10] { "1A", "7A", "2A", "8A", "6A", "4A", "8A", "3A", "7A", "2A" }; // corretas
+    public static string[] family_alt1 = new string[10] { "2A", "6A", "3A", "1A", "5A", "5A", "7A", "2A", "8A", "3A" }; // alternativas 1
+    public static string[] family_alt2 = new string[10] { "3A", "5A", "4A", "2A", "4A", "3A", "6A", "5A", "6A", "4A" }; // alternativas 2
+    #endregion
+}
---
+++ Smart Chemistry/Smart Chemistry/Assets/Resources/Scripts/Shake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public static bool shake; // check camera shake
+    public static float shake_Time, shake_Intensity; // shake time and intensity
+
+    void Update()
+    {
+        #region CAMERA SHAKE
+        if (shake == true && shake_Time > 0)
+        {
+            Vector2 shakeMe = Random.insideUnitCircle * shake_Intensity;
+            transform.position = new Vector3(transform.position.x + shakeMe.x, transform.position.y + shakeMe.y, transform.position.z);
+            shake_Time -= Time.deltaTime;
+        }
+        #endregion
+    }
+}

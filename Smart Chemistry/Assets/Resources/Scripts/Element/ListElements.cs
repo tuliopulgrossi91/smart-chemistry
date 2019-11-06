44e70dcbb541ef74dceb4c704def390ab90a420d
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2019-11-05 22:35:02 -0300

    add elements json

--- Smart Chemistry/Assets/Resources/Scripts/Element/ListElements.cs
+++ Smart Chemistry/Assets/Resources/Scripts/Element/ListElements.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 using System;
 public class ListElements : MonoBehaviour
 {
     string path;
     string jsonString;
-
+    public GameObject ElementDrag;
+    public Transform transformParent;
     void Start()
     {
         path = Application.dataPath + "/Resources/Json/elements.json";
         jsonString = File.ReadAllText(path);
         ElementsCollection elementsCollection = JsonUtility.FromJson<ElementsCollection>(jsonString);
+
+        //Create Objects
+        for (int i = 1; i < elementsCollection.elements.Length; i++)
+        {
+            GameObject elementClone = Instantiate(ElementDrag, transformParent);
+            elementClone.transform.GetChild(0).GetComponent<Text>().text = elementsCollection.elements[i].symbol;
+        }
+
+        //Find Element
         Element element = this.findByAtomicNumber(1, elementsCollection.elements);
         Debug.Log(element.name);
         Debug.Log(element.symbol);
@@ -23,7 +34,7 @@ public class ListElements : MonoBehaviour
     public Element findByAtomicNumber(int atomicNumber, Element[] elements)
     {
         int pos = 0;
-        pos = Array.FindIndex(elements , (x) => (x.atomicNumber == atomicNumber));
+        pos = Array.FindIndex(elements, (x) => (x.atomicNumber == atomicNumber));
         return elements[pos];
     }
 }
